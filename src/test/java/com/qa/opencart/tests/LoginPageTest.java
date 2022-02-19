7c393f70aa01050687ad2f65152029d976ea8273
coderboy-ak <study24forall@gmail.com>
2022-02-19 13:21:04 +0530

    added and updated code

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -1,11 +1,24 @@
 package com.qa.opencart.tests;
 
+import com.qa.opencart.listeners.AnnotationTransformer;
+import com.qa.opencart.listeners.TestAllureListener;
 import com.qa.opencart.utils.Constants;
+import io.qameta.allure.*;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Epic("EPIC - 100 : Design Login page for Open Cart Application....")
+
+@Story("US - 101 : Login Page Features")
+
+@Listeners({AnnotationTransformer.class, TestAllureListener.class})
 public class LoginPageTest extends BaseTest {
 
+    //Allure Report
+    @Description("TC_01: login Page Title Test")
+    @Severity(SeverityLevel.MINOR)
+    //@Test(enabled = false)   //this test will not execute
     @Test
     public void loginPageTitleTest() {
         String title = loginPage.getLoginPageTitle();
@@ -13,6 +26,8 @@ public class LoginPageTest extends BaseTest {
         Assert.assertEquals(title, Constants.LOGIN_PAGE_TITLE);
     }
 
+    @Description("login page url test")
+    @Severity(SeverityLevel.NORMAL)
     @Test
     public void loginPageURLTest() {
         String url = loginPage.getLoginPageUrl();
@@ -20,18 +35,20 @@ public class LoginPageTest extends BaseTest {
         Assert.assertTrue(url.contains(Constants.LOGIN_PAGE_URL_FRACTION)); //boolean value
     }
 
+    @Description("this is testing forgot pwd link on login page")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void forgotPwdLinkTest() {
         Assert.assertTrue(loginPage.isForgotPwdLinkExist());
+        //Assert.assertTrue(false);
     }
 
-    @Test
+    @Description("positive test case for login with correct credentials...")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "login test with correct username and password")
     public void loginTest() {
         accPage = loginPage.doLogin(prop.getProperty("username").trim(), prop.getProperty("password").trim());
         Assert.assertTrue(accPage.isNewsletterLinkExist());
     }
-
-
-
 
 }
