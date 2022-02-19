bb93827159970b3f24bca509d8c659bb4b4d1f30
coderboy-ak <study24forall@gmail.com>
2022-02-19 19:56:11 +0530

    order page is updated as per Naveen  review comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,4 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
+
+    By order = By.id("order");
+
+    public void order(){
+        System.out.println("order id : 12345");
+    }
+
 }
